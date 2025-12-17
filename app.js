81f57bf0f5676773af68bc95d28e73f12cb8dcad
kudasiv2 <fadliahlam919@gmail.com>
2025-12-17 18:07:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-[file name]: app.js
-[file content begin]
-// Updated contract address and ABI for new contract
 const CONTRACT_ADDRESS = '0x3eCCC758B9a82D2eB543Dc13433B9beaA46baBF9';
 const USDT_ADDRESS = '0x55d398326f99059fF775485246999027B3197955';
 const READ_ONLY_RPC = 'https://bsc-dataseed1.binance.org/';
